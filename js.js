6d80ca3d95fd7658e9e8a5401d339b2456d0b544
anassaleh218 <anassaleh035@gmail.com>
2023-05-16 00:54:58 +0300

    responsive front-end website

---
+++ js.js
@@ -0,0 +1,14 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("ham").style.display = "none";
+  document.getElementById("close").style.display="block";
+}
+
+
+function closeNav() {
+  document.getElementById("mySidenav").style.display = "none";
+  document.getElementById("ham").style.display = "block";
+  document.getElementById("close").style.display="none";
+}
+
